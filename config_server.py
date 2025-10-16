bf069e8079aec357d064c920f5ec869cd2962622
NiltonCastro <junior02@gmail.com>
2025-10-16 17:21:55 -0300

    Atualizacao do path no firefox

--- config_server.py
+++ config_server.py
@@ -1,6 +1,6 @@
 def paths(parameter):
     mapping = {
         "db_path": "/home/jonbot/jonbot-fifa/database/jonbet-bot.db",
-        "fire_fox": "/usr/bin/firefox",
+        "fire_fox": "/snap/firefox/current/usr/lib/firefox/firefox",
     }
     return mapping.get(parameter, parameter)
